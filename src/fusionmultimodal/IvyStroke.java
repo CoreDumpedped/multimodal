95751259394e10ef6b46cf765a35ba32517e3f01
unknown <astierre@ENAC-IHM18.ihm>
2016-01-22 16:50:09 +0100

    sa marche avant refactore (arnaud va toucher le code donc je prefer commit

--- src/fusionmultimodal/IvyStroke.java
+++ src/fusionmultimodal/IvyStroke.java
@@ -27,20 +27,21 @@ import javafx.scene.input.DragEvent;
  */
 public class IvyStroke {
 
-
-
     public Ivy bus;
     public List<Point2D.Double> listPoints;
 
     private enum MouseState {
+
         drag, release
     };
-    
-    private enum VocalStat{
-        init,wait
+
+    private enum VocalStat {
+
+        init, wait
     }
 
     private enum State {
+
         save, run
     };
     private State state = State.save;
@@ -51,14 +52,15 @@ public class IvyStroke {
     private Point dernierPoint;
 
     Recognizer recognizer;
+
     public IvyStroke() throws IvyException {
-        vocalStat=VocalStat.init;
-        recognizer=new Recognizer();
+        vocalStat = VocalStat.init;
+        recognizer = new Recognizer();
         etat = MouseState.release;
         listPoints = new ArrayList<>();
-        
+        dernierPoint=new Point(0, 0);
         bus = new Ivy("IvyStroke", "IvyStroke Ready", null);
-       
+
         bus.bindMsg("^Palette:mouseDragged x=(.*) y=(.*)", new IvyMessageListener() {
             public void receive(IvyClient client, String[] args) {
                 if (etat == MouseState.release) {
@@ -70,89 +72,83 @@ public class IvyStroke {
                 Point2D.Double p = new Point2D.Double(x, y);
                 // listPoints.add(new Point2D.Double(x, y));
                 s.addPoint(p);
-                vocalStat=VocalStat.init;
+                vocalStat = VocalStat.init;
             }
         });
-        
-        
-                bus.bindMsg("^Palette:mousePressed x=(.*) y=(.*)", new IvyMessageListener() {
+
+        bus.bindMsg("^Palette:MousePressed x=(.*) y=(.*)", new IvyMessageListener() {
             public void receive(IvyClient client, String[] args) {
-               
+                System.err.println("recup=" + dernierPoint.x );
+                dernierPoint.x=Integer.parseInt(args[0]);
+                dernierPoint.y=Integer.parseInt(args[1]);
             }
         });
 
         bus.bindMsg("^Palette:MouseReleased x=(.*) y=(.*)", new IvyMessageListener() {
             public void receive(IvyClient client, String[] args) {
-                
+
                 etat = MouseState.release;
                 if (state == State.save) {
-                       recognizer.addTemplates(new Template("oval", s));
-                       recognizer.saveTemplates();
+                    recognizer.addTemplates(new Template("oval", s));
+                    recognizer.saveTemplates();
                 } else {
                     System.out.println("START Verif Stroke ");
-                   s.normalize();
-                   recognizer.setStrokeCourant(s);
-                   Template t = recognizer.verifStroke();
-                   if (t != null) {
-                       System.out.println("Cette figure ressemblerai a s'y méprendre (et c'est peu de le dire !) à un :" + t.getNom());
-                     //  dessin(t);
-                       templateEnAttente=t;
-                       vocalStat=VocalStat.wait;
-                   }
-                }                
+                    s.normalize();
+                    recognizer.setStrokeCourant(s);
+                    Template t = recognizer.verifStroke();
+                    if (t != null) {
+                        System.out.println("Cette figure ressemblerai a s'y méprendre (et c'est peu de le dire !) à un :" + t.getNom());
+                        //  dessin(t);
+                        templateEnAttente = t;
+                        vocalStat = VocalStat.wait;
+                    }
+                }
             }
         });
 
-        
-        
-          bus.bindMsg("^sra5 Text=(.*) Confidence", new IvyMessageListener() { //vocal
+        bus.bindMsg("^sra5 Text=(.*) Confidence", new IvyMessageListener() { //vocal
             public void receive(IvyClient client, String[] args) {
                 System.out.println("je suis passer par ici:" + args[0]);
-                   if(args[0].equals("ici") ||args[0].equals("la") ||args[0].equals("... ici") ||args[0].equals("... la")  ){
-                       switch(vocalStat){
-                           case wait:        
-                               dessin(templateEnAttente);
-                               vocalStat=VocalStat.init;
-                               break;
-                           case init: break;  
-                       }
-                        
+                if (args[0].equals("ici") || args[0].equals("la") || args[0].equals("... ici") || args[0].equals("... la")) {
+                    switch (vocalStat) {
+                        case wait:
+                            dessin(templateEnAttente);
+                            vocalStat = VocalStat.init;
+                            break;
+                        case init:
+                            break;
                     }
+
+                }
             }
         });
-        
-        
-        
+
         bus.start(null);
     }
 
-    
-    
-    
-    
-    public void dessin(Template t){   
+    public void dessin(Template t) {
         System.out.println("dessine moi un " + t.getNom());
-     try {
-        switch (t.getNom()) {
-            case "carre" : 
-                bus.sendMsg("Palette:CreerRectangle x=10 y=20");
-                break;
-            case "oval" : 
-                bus.sendMsg("Palette:CreerEllipse x=10 y=20");
+        try {
+            switch (t.getNom()) {
+                case "carrer":
+                    bus.sendMsg("Palette:CreerRectangle x="+dernierPoint.x +" y="+dernierPoint.y);
+                    break;
+                case "oval":
+                    bus.sendMsg("Palette:CreerEllipse x="+dernierPoint.x +" y="+dernierPoint.y);
+            }
+
+            System.out.println("all is ok");
+        } catch (IvyException ie) {
+            System.out.println("can't send my message !");
         }
-      
-       System.out.println("all is ok");
-    } catch (IvyException ie) {
-      System.out.println("can't send my message !");
+
     }
-        
-    }
-    
-      public void setRunState() {
+
+    public void setRunState() {
         state = State.run;
         System.out.println("Etat RUN activé !");
     }
-    
+
     private void envoieStroke() {
         s.normalize();
     }
